f01f6c852bb959401aa7a7cd6fd7b47df0f5b602
Brad Hamill <brad@hamill.com>
2020-12-09 00:14:49 -0700

    up to start of tickets

--- auth/src/routes/__test__/currentUser.test.ts
+++ auth/src/routes/__test__/currentUser.test.ts
@@ -1,6 +1,5 @@
 import request from 'supertest'
 import { app } from '../../app'
-import { signupRouter } from '../signup'
 
 it('responds with details about the current user', async () => {
   const cookie = await global.signup()
