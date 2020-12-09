4e66f4308351482db15645837fc70ade189a5a49
Brad Hamill <brad@hamill.com>
2020-12-08 17:13:30 -0700

    done with next app auth

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,30 +1,31 @@
 import 'bootstrap/dist/css/bootstrap.css'
 
 import buildClient from '../api/build-client'
+import Header from '../components/header'
 
-const AppComponent = ({ Component, pageProps }) => {
+const AppComponent = ({ Component, pageProps, currentUser }) => {
   return (
     <div>
-      <h1>Header!</h1>
+      <Header currentUser={currentUser} />
       <Component {...pageProps} />
     </div>
   )
 }
 
-// AppComponent.getInitialProps = async (appContext) => {
-//   const client = buildClient(appContext.ctx)
-//   const { data } = await client.get('/api/users/getcurrentuser')
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx)
+  const { data } = await client.get('/api/users/currentuser')
 
-//   let pageProps = {}
+  let pageProps = {}
 
-//   if (appContext.Component.getInitialProps) {
-//     pageProps = await appContext.Component.getInitialProps(appContext.ctx)
-//   }
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx)
+  }
 
-//   return {
-//     pageProps,
-//     ...data,
-//   }
-// }
+  return {
+    pageProps,
+    ...data,
+  }
+}
 
 export default AppComponent
